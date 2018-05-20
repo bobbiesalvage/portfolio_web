0c6e4f83fa4e4e37d17f0eca84703b7e916895c1
bobbiesalvage <kelly-oflaherty@hotmail.com>
2018-05-20 23:26:01 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,79 @@
+// get all images from images folder //
+
+var imagesInFolder = [];
+
+function loadImages () {
+	// create array 
+	//let imagesInFolder = [];
+	// load images into array
+	
+	// load images from images folder
+	const folder = "./images";
+	const fs = require('fs');
+
+	fs.readdirSync(folder).forEach(file => {
+		imagesInFolder += file;
+		return imagesInFolder;
+		//console.log(file);
+	})
+	
+}
+
+
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+// add images for length of array
+//<div class="column">
+  //  <img src="images/img_nature.jpg" style="width:100%" onclick="openModal();currentSlide(1)" class="hover-shadow cursor">
+  //</div>
+
+function createImages () {
+	for (var value of imagesInFolder){
+		var createDiv = document.createElement("div");
+		var x = document.createElement("IMG");
+		x.setAttribute("src", "imagesInFolder[value]");
+		x.setAttribute("width", "304");
+		x.setAttribute("height", "228");
+		x.setAttribute("alt", "Image 1");
+		x.setAttribute("onclick", "openModal();currentSlide(value)");
+		x.setAttribute("class", "hover-shadow cursor");
+		createDiv.appendChild(x);
+		document.body.appendChild(createDiv);
+	}
+}  
+  
+  
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
